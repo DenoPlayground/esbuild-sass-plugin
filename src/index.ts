8b56ef3327c98a2deaf3805832bf8862bd820fa0
Mqx <62719703+Mqxx@users.noreply.github.com>
2024-01-10 11:46:08 +0100

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,20 @@
+import * as esbuild from "https://deno.land/x/esbuild@v0.19.11/mod.js";
+import sass from "https://deno.land/x/denosass@1.0.6/mod.ts"
 
+const sassPlugin: esbuild.Plugin = {
+    name: "esbuild-plugin-sass",
+    setup: (build) => {
+        build.onLoad(
+            { filter: /\.scss$/ },
+            async (args) => {
+                const file = await Deno.readTextFile(args.path)
+                const css = sass(file).to_string(build.initialOptions.minify ? 'compressed' : 'expanded')
+                
+                return {
+                    contents: css.toString(),
+                    loader: 'css'
+                }
+            }
+        )
+    }
+}
