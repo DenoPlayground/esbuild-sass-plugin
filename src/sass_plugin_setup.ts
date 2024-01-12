4130f35348468d9052ca34570092428bbe0c4213
Mqxx <62719703+Mqxx@users.noreply.github.com>
2024-01-12 07:02:57 +0100

    Update sass_plugin_setup.ts

--- src/sass_plugin_setup.ts
+++ src/sass_plugin_setup.ts
@@ -1,3 +1,4 @@
+import { dirname } from "https://deno.land/std@0.211.0/path/dirname.ts";
 import sass from 'https://deno.land/x/denosass@1.0.6/mod.ts';
 import {
   BuildOptions,
@@ -29,7 +30,7 @@ export default function sassPluginSetup(
   onLoadFunction(
     { filter: /\.scss$/ },
     async (args) => {
-      const path = new URL('.', args.path).pathname;
+      const path = dirname(args.path);
       
       const fileContent = await Deno.readTextFile(args.path);
 
